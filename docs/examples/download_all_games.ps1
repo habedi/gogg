f2b09ec31172c0b03c56f1138f5bac2f428ac02c
Hassan Abedi <hassan.abedi.t@gmail.com>
2025-01-17 17:21:29 +0100

    WIP

--- docs/examples/download_all_games.ps1
+++ docs/examples/download_all_games.ps1
@@ -12,7 +12,7 @@ Write-Host "${GREEN}The code in this script downloads all games owned by the use
 Write-Host "${GREEN}============================================================================================${NC}"
 
 $DEBUG_MODE = 1 # Debug mode enabled
-$GOGG = (Get-Command "bin\gogg" -ErrorAction SilentlyContinue) -or (Get-Command "gogg" -ErrorAction SilentlyContinue)
+$GOGG = ".\gogg" # Path to Gogg's executable file (for example, ".\bin\gogg")
 
 $LANG = "en" # Language English
 $PLATFORM = "windows" # Platform Windows
@@ -22,18 +22,17 @@ $RESUME_DOWNLOAD = 1 # Resume download
 $NUM_THREADS = 4 # Number of worker threads for downloading
 
 # Function to clean up the CSV file
-function Cleanup {
-    if ($latest_csv) {
+function Cleanup
+{
+    if ($latest_csv)
+    {
         Remove-Item -Force $latest_csv
-        if ($?) {
+        if ($?)
+        {
             Write-Host "${RED}Cleanup: removed $latest_csv${NC}"
         }
     }
 }
-
-# Trap Ctrl+C and call Cleanup
-$global:latest_csv = $null
-Register-ObjectEvent -InputObject $Host -EventName "CancelKeyPress" -Action { Cleanup }
 
 # Update game catalogue and export it to a CSV file
 & $GOGG catalogue refresh
@@ -43,12 +42,13 @@ Register-ObjectEvent -InputObject $Host -EventName "CancelKeyPress" -Action { Cl
 $latest_csv = Get-ChildItem -Path . -Filter "gogg_catalogue_*.csv" | Sort-Object LastWriteTime -Descending | Select-Object -First 1
 
 # Check if the catalogue file exists
-if (-not $latest_csv) {
+if (-not $latest_csv)
+{
     Write-Host "${RED}No CSV file found.${NC}"
     exit 1
 }
 
-Write-Host "${GREEN}Using catalogue file: $($latest_csv.Name)${NC}"
+Write-Host "${GREEN}Using catalogue file: $( $latest_csv.Name )${NC}"
 
 # Download each game listed in catalogue file, skipping the first line
 Get-Content $latest_csv.FullName | Select-Object -Skip 1 | ForEach-Object {
@@ -56,10 +56,10 @@ Get-Content $latest_csv.FullName | Select-Object -Skip 1 | ForEach-Object {
     $game_id = $fields[0]
     $game_title = $fields[1]
     Write-Host "${YELLOW}Game ID: $game_id, Title: $game_title${NC}"
+    $env:DEBUG_GOGG = $DEBUG_MODE
     & $GOGG download --id $game_id --dir "./games" --platform $PLATFORM --lang $LANG `
         --dlcs $INCLUDE_DLC --extras $INCLUDE_EXTRA_CONTENT --resume $RESUME_DOWNLOAD --threads $NUM_THREADS
     Start-Sleep -Seconds 1
-    # Remove the break to download all games
 }
 
 # Clean up
